9864c068654f7e27a03ba5f1db1171f300c704f8
Kirk Ouimet <kirk@kirkouimet.com>
2024-03-23 09:02:51 -0600

    Adding script to sync with GraphQL API

---
+++ scripts/api.sh
@@ -0,0 +1,39 @@
+# Store the projet directory
+projectDirectory=$PWD
+
+# Echo
+echo "Updating GraphQL generated code..."
+
+# Change to the api directory and pull the latest code
+echo "Changing to the api directory..."
+cd ../api
+apiDirectory=$PWD
+
+echo "Pulling the latest api code..."
+git pull
+
+# Change to the base directory and pull the latest code
+echo "Changing to the base library directory..."
+cd libraries/base
+baseLibraryDirectory=$PWD
+
+echo "Pulling the latest base library code..."
+git pull
+
+# Generating the base library GraphQL code
+echo "Changing to the api directory..."
+cd ../../
+echo "Generating base library GraphQL code..."
+node base graphql schema:generate -w graphql -s
+
+# Copy the files from apiDirectory/workers/graphql/graphql/schemas to projectDirectory/source/api/schemas
+echo "Copying the generated GraphQL files to the project directory..."
+cp -r $apiDirectory/workers/graphql/graphql/schemas $projectDirectory/source/api/
+
+# Change back to the project directory
+echo "Changing back to the project directory..."
+cd $projectDirectory
+
+# Generate GraphQL code
+echo "Generating project GraphQL code..."
+npm run graphql:generate
