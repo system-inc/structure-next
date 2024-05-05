186f01cff8d3126840c3005cf91a55fed39b5a66
Kirk Ouimet <kirk@kirkouimet.com>
2024-05-04 20:09:30 -0600

    Creating LocaleControl and adding icons for platforms

---
+++ source/common/navigation/LocaleControl.tsx
@@ -0,0 +1,34 @@
+'use client'; // This component uses client-only features
+
+// Dependencies - React and Next.js
+import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+// Dependencies - Main Components
+
+// Dependencies - Assets
+import CurrentLocationIcon from '@structure/assets/icons/navigation/CurrentLocationIcon.svg';
+
+// Dependencies - Utilities
+import { mergeClassNames } from '@structure/source/utilities/Styles';
+
+// Component - LocaleControl
+export interface LocaleControlInterface {
+    className?: string;
+}
+export function LocaleControl(properties: LocaleControlInterface) {
+    // Hooks
+    const urlPathname = usePathname();
+
+    // Render the component
+    return (
+        <div className={mergeClassNames(properties.className, 'flex cursor-pointer items-center')}>
+            <CurrentLocationIcon className="mr-2 h-4 w-4" />
+            <span>English (United States)</span>
+        </div>
+    );
+}
+
+// Export - Default
+export default LocaleControl;
