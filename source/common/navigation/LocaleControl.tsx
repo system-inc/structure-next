53af0b8420d1c5681679ceac86a0779be6b6a9ca
Andrew Jacobs <magicjaqk@gmail.com>
2024-05-13 10:27:54 -0600

    Update locale changing to use the locale change hook rather than links.

--- source/common/navigation/LocaleControl.tsx
+++ source/common/navigation/LocaleControl.tsx
@@ -12,8 +12,9 @@ import CurrentLocationIcon from '@structure/assets/icons/navigation/CurrentLocat
 
 // Dependencies - Utilities
 import { mergeClassNames } from '@structure/source/utilities/Styles';
-import { useI18n } from '@project/locales/client';
+import { useChangeLocale, useI18n } from '@project/locales/client';
 import Popover from '../popovers/Popover';
+import Button from '../buttons/Button';
 
 // Component - LocaleControl
 export interface LocaleControlInterface {
@@ -22,6 +23,7 @@ export interface LocaleControlInterface {
 export function LocaleControl(properties: LocaleControlInterface) {
     // Hooks
     const translation = useI18n();
+    const changeLocale = useChangeLocale();
     const pathname = usePathname();
     console.log(pathname);
     const pathnameWithoutLocale = pathname.replace(/^\/[a-z]{2}\//, '/');
@@ -33,20 +35,20 @@ export function LocaleControl(properties: LocaleControlInterface) {
             align="end"
             content={
                 <div className="flex flex-col p-2">
-                    <Link
-                        href={'/en' + pathnameWithoutLocale}
-                        locale="en"
-                        className="rounded-md px-2 py-1 text-dark hover:bg-light hover:text-dark"
+                    <Button
+                        variant="unstyled"
+                        className="my-0.5 rounded-sm text-left text-sm font-medium transition-colors hover:bg-dark/10"
+                        onClick={() => changeLocale('en')}
                     >
                         English
-                    </Link>
-                    <Link
-                        href={'/es' + pathnameWithoutLocale}
-                        locale="es"
-                        className="rounded-md px-2 py-1 text-dark hover:bg-light hover:text-dark"
+                    </Button>
+                    <Button
+                        variant="unstyled"
+                        className="my-0.5 rounded-sm text-left text-sm font-medium transition-colors hover:bg-dark/10"
+                        onClick={() => changeLocale('es')}
                     >
                         Español
-                    </Link>
+                    </Button>
                 </div>
             }
         >
