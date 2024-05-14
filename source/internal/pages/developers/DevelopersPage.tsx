2ba5a8f480b5a1ab971619c9db77c062d78583ca
Andrew Jacobs <magicjaqk@gmail.com>
2024-05-13 21:22:23 -0600

    feat: Add CodeEditor component to DevelopersPage

--- source/internal/pages/developers/DevelopersPage.tsx
+++ source/internal/pages/developers/DevelopersPage.tsx
@@ -1,18 +1,26 @@
+'use client';
+
 // Dependencies - React and Next.js
 import React from 'react';
 
 // Dependencies - Main Components
 import InternalNavigationTrail from '@structure/source/internal/layouts/navigation/InternalNavigationTrail';
+import CodeEditor from '@project/source/modules/common/text/CodeView';
 
 // Component - DevelopersPage
 export type DevelopersPageProperties = {};
 export function DevelopersPage(properties: DevelopersPageProperties) {
+    const [code, setCode] = React.useState<string>(`// Code goes here
+const [state, setState] = React.useState<string>('');`);
+
     // Render the component
     return (
         <>
             <InternalNavigationTrail />
 
             <h1>Developers</h1>
+
+            <CodeEditor code={code} setCode={setCode} language="ts" className="text-sm" edit />
         </>
     );
 }
