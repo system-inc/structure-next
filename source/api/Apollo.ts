b9bb63c4a11879a922e4934e5f05ec3ddf80f956
Kirk Ouimet <kirk@kirkouimet.com>
2024-08-06 17:20:15 -0600

    Updating Apollo to not cache server side requests

--- source/api/Apollo.ts
+++ source/api/Apollo.ts
@@ -10,6 +10,21 @@ function getApolloClientHttpLink(mode: 'Browser' | 'Server') {
     return createHttpLink({
         uri: StructureSettings.apis.base.url + 'graphql', // This needs to be an absolute url, as relative urls cannot be used in SSR
         credentials: mode === 'Browser' ? 'include' : undefined, // Cloudflare Workers does not support 'include'
+        // Disable caching for server-side rendering for development
+        fetch:
+            mode === 'Server'
+                ? function (uri, options) {
+                      return fetch(uri, {
+                          ...(options ?? {}),
+                          headers: {
+                              ...(options?.headers ?? {}),
+                          },
+                          next: {
+                              revalidate: 0,
+                          },
+                      });
+                  }
+                : undefined,
     });
 }
 
@@ -25,4 +40,15 @@ export const serverSideApolloClient = new ApolloClient({
     link: getApolloClientHttpLink('Server'),
     cache: new InMemoryCache(),
     ssrMode: true,
+    // Disable caching for server-side rendering for development
+    defaultOptions: {
+        query: {
+            fetchPolicy: 'no-cache',
+            errorPolicy: 'all',
+        },
+        watchQuery: {
+            fetchPolicy: 'no-cache',
+            errorPolicy: 'all',
+        },
+    },
 });
---
+++ source/modules/post/ProfileLink.tsx
@@ -0,0 +1,51 @@
+'use client'; // This component uses client-only features
+
+// Dependencies - React and Next.js
+import React from 'react';
+import Link from 'next/link';
+
+// Dependencies - Main Components
+import ProfileImage from '@structure/source/modules/account/ProfileImage';
+
+// Dependencies - Utilities
+import { mergeClassNames } from '@structure/source/utilities/Style';
+
+// Component - ProfileLink
+export interface ProfileLinkInterface {
+    className?: string;
+    username: string;
+    displayName?: string;
+    imageUrls?: {
+        url: string;
+        variant: string;
+    }[];
+}
+export function ProfileLink(properties: ProfileLinkInterface) {
+    // Defaults
+    const displayName = properties.displayName || properties.username;
+
+    // Get the profile image URL
+    const profileImageUrl = properties.imageUrls?.find((image) => image.variant === 'profile-image-small')?.url;
+
+    // Render the component
+    return (
+        <Link
+            className={mergeClassNames(
+                'group flex items-center space-x-2 text-neutral-6 transition-colors hover:text-dark-6 active:text-dark dark:text-light-3 dark:hover:text-light-2 dark:active:text-light',
+                properties.className,
+            )}
+            href={`/profiles/${properties.username}`}
+        >
+            {/* Profile Image */}
+            <div className="h-7 w-7">
+                <ProfileImage profileImageUrl={profileImageUrl} alternateText={displayName} />
+            </div>
+
+            {/* Display Name */}
+            <div className="group-hover:underline">{displayName}</div>
+        </Link>
+    );
+}
+
+// Export - Default
+export default ProfileLink;
