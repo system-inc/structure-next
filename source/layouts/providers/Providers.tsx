0561b6a1539a95a4e99bc047b1700736371432b8
Andrew Jacobs <38973832+magicjaqk@users.noreply.github.com>
2024-10-23 14:04:06 -0600

    Removing NuqsAdapter to complete reverting changes.

--- source/layouts/providers/Providers.tsx
+++ source/layouts/providers/Providers.tsx
@@ -13,7 +13,6 @@ import SharedStateProvider from '@structure/source/utilities/shared-state/Shared
 import EngagementProvider from '@structure/source/modules/engagement/EngagementProvider';
 import NoticeProvider from '@structure/source/common/notifications/NoticeProvider';
 import TipProvider from '@structure/source/common/popovers/TipProvider';
-import { NuqsAdapter } from 'nuqs/adapters/next/app';
 
 export interface ProvidersInterface {
     children: React.ReactNode;
@@ -24,21 +23,19 @@ export function Providers(properties: ProvidersInterface) {
     // Render the component
     return (
         <CookiesProvider cookies={Cookies}>
-            <NuqsAdapter>
-                <ApolloProvider>
-                    <ThemeProvider themeClassName={properties.themeClassName}>
-                        <AccountProvider signedIn={properties.accountSignedIn}>
-                            <SharedStateProvider>
-                                <EngagementProvider>
-                                    <NoticeProvider>
-                                        <TipProvider delayDuration={100}>{properties.children}</TipProvider>
-                                    </NoticeProvider>
-                                </EngagementProvider>
-                            </SharedStateProvider>
-                        </AccountProvider>
-                    </ThemeProvider>
-                </ApolloProvider>
-            </NuqsAdapter>
+            <ApolloProvider>
+                <ThemeProvider themeClassName={properties.themeClassName}>
+                    <AccountProvider signedIn={properties.accountSignedIn}>
+                        <SharedStateProvider>
+                            <EngagementProvider>
+                                <NoticeProvider>
+                                    <TipProvider delayDuration={100}>{properties.children}</TipProvider>
+                                </NoticeProvider>
+                            </EngagementProvider>
+                        </SharedStateProvider>
+                    </AccountProvider>
+                </ThemeProvider>
+            </ApolloProvider>
         </CookiesProvider>
     );
 }
