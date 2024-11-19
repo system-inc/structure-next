f0b6a973320774ebc51151f48950c4fbb82c8f8f
kirkouimet <kirk@kirkouimet.com>
2024-11-19 16:55:02 -0700

    Working on getting service bindings working. It appears that we do not need to apply the type policy as id is always a key field

--- source/api/Apollo.ts
+++ source/api/Apollo.ts
@@ -4,62 +4,23 @@ import ProjectSettings from '@project/ProjectSettings';
 // Dependencies - API
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
-// Function to create an HttpLink for Apollo
-function getApolloClientHttpLink(mode: 'Browser' | 'Server') {
-    // Create the link to the API
-    return createHttpLink({
-        uri: ProjectSettings.apis.base.url + 'graphql', // This needs to be an absolute url, as relative urls cannot be used in SSR
-        credentials: mode === 'Browser' ? 'include' : undefined, // Cloudflare Workers does not support 'include'
-        // Disable caching for server-side rendering for development
-        // fetch:
-        //     mode === 'Server'
-        //         ? function (uri, options) {
-        //               return fetch(uri, {
-        //                   ...(options ?? {}),
-        //                   headers: {
-        //                       ...(options?.headers ?? {}),
-        //                   },
-        //                   next: {
-        //                       //       revalidate: 0,
-        //                   },
-        //               });
-        //           }
-        //         : undefined,
-    });
-}
-
-// Apollo client
-const apolloClientCache = new InMemoryCache({
-    typePolicies: {
-        ShoppingBagItem: {
-            keyFields: ['id'],
-        },
-    },
-});
-
 // Create a globally shared Apollo client for the browser
 export const apolloClient = new ApolloClient({
-    link: getApolloClientHttpLink('Browser'),
-    cache: apolloClientCache,
+    cache: new InMemoryCache(),
+    link: createHttpLink({
+        uri: ProjectSettings.apis.base.url + 'graphql', // This needs to be an absolute url, as relative urls cannot be used in SSR
+        credentials: 'include', // Include HTTP Only cookies
+    }),
 });
 
 // Function to get an Apollo client for server-side rendering
 // Having a globally shared Apollo client breaks Cloudflare, so we need to create a new Apollo client for each request
 export const getApolloClientForServerSideRendering = function () {
     return new ApolloClient({
-        link: getApolloClientHttpLink('Server'),
-        cache: new InMemoryCache(),
         ssrMode: true,
-        // Disable caching for server-side rendering for development
-        // defaultOptions: {
-        //     query: {
-        //         fetchPolicy: 'no-cache',
-        //         errorPolicy: 'all',
-        //     },
-        //     watchQuery: {
-        //         fetchPolicy: 'no-cache',
-        //         errorPolicy: 'all',
-        //     },
-        // },
+        cache: new InMemoryCache(),
+        link: createHttpLink({
+            uri: ProjectSettings.apis.base.url + 'graphql', // This needs to be an absolute url, as relative urls cannot be used in SSR
+        }),
     });
 };
