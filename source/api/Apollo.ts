95e709f618736e4751968958a62e1ec04777948e
kirkouimet <kirk@kirkouimet.com>
2024-11-19 17:45:00 -0700

    Using Cloudflare service bindings to massively speed up worker to worker calls on production only

--- source/api/Apollo.ts
+++ source/api/Apollo.ts
@@ -12,15 +12,3 @@ export const apolloClient = new ApolloClient({
         credentials: 'include', // Include HTTP Only cookies
     }),
 });
-
-// Function to get an Apollo client for server-side rendering
-// Having a globally shared Apollo client breaks Cloudflare, so we need to create a new Apollo client for each request
-export const getApolloClientForServerSideRendering = function () {
-    return new ApolloClient({
-        ssrMode: true,
-        cache: new InMemoryCache(),
-        link: createHttpLink({
-            uri: ProjectSettings.apis.base.url + 'graphql', // This needs to be an absolute url, as relative urls cannot be used in SSR
-        }),
-    });
-};
---
+++ source/api/ApolloSsr.ts
@@ -0,0 +1,42 @@
+// Dependencies - Project
+import ProjectSettings from '@project/ProjectSettings';
+
+// Dependencies - API
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { getCloudflareContext } from '@opennextjs/cloudflare';
+
+// Function to get an Apollo client for server-side rendering
+// Having a globally shared Apollo client breaks Cloudflare, so we need to create a new Apollo client for each request
+export const getApolloClientForServerSideRendering = function () {
+    console.log('process.env.NEXT_PHASE', process.env.NEXT_PHASE);
+
+    // If in development environment or building for production
+    if(process.env.NODE_ENV === 'development' || process.env.NEXT_PHASE === 'phase-production-build') {
+        // Return a standard Apollo client
+        return new ApolloClient({
+            ssrMode: true,
+            cache: new InMemoryCache(),
+            link: createHttpLink({
+                uri: ProjectSettings.apis.base.url + 'graphql', // This needs to be an absolute url, as relative urls cannot be used in SSR
+            }),
+        });
+    }
+    // If in production environment
+    else {
+        // Return an Apollo client that uses the bound API worker
+        // This is much faster, doesn't bill us for two worker invocations, and does not create new deviceIds
+        return new ApolloClient({
+            ssrMode: true,
+            cache: new InMemoryCache(),
+            link: createHttpLink({
+                fetch: async function (uri, options) {
+                    const cloudflareContext = await getCloudflareContext();
+                    const url = 'https://website.base-internal' + uri;
+                    // Ignore the any
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    return (cloudflareContext.env as any).api.fetch(url, options);
+                },
+            }),
+        });
+    }
+};
