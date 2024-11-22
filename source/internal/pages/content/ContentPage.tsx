894f5b8bf8ee2ed2958dc8b748ca49cba7691407
Andrew Jacobs <38973832+magicjaqk@users.noreply.github.com>
2024-11-22 16:58:44 -0700

    Adding MD table support to Markdown renderer component. Demo on `/internal/content`.

--- source/internal/pages/content/ContentPage.tsx
+++ source/internal/pages/content/ContentPage.tsx
@@ -3,15 +3,29 @@ import React from 'react';
 
 // Dependencies - Main Components
 import InternalNavigationTrail from '@structure/source/internal/layouts/navigation/InternalNavigationTrail';
+import Markdown from '@structure/source/common/markdown/Markdown';
 
 // Component - ContentPage
-export type ContentPageProperties = {};
-export function ContentPage(properties: ContentPageProperties) {
+export function ContentPage() {
     // Render the component
+
+    const tableTest = `
+# Table Test
+
+| Energy Amount | OpenAI GPT-4o (default) | Anthropic Claude Sonnet 3.5 | Anthropic Claude Opus |
+|--------------|-------------------------|---------------------------|---------------------|
+| 1B | 74 messages | 56 messages | 11 messages |
+| 2B | 148 messages | 111 messages | 22 messages |
+| 5B | 370 messages | 278 messages | 56 messages |
+| 10B | 741 messages | 556 messages | 111 messages |
+| 11B | 815 messages | 611 messages | 122 messages |
+  `;
+
     return (
         <div className="px-6 py-4">
             <InternalNavigationTrail />
             <h1>Content</h1>
+            <Markdown className="mt-8">{tableTest}</Markdown>
         </div>
     );
 }
