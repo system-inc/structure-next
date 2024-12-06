96f5a038461340ba5b38fc33b5e280064a31f6e9
Andrew Jacobs <38973832+magicjaqk@users.noreply.github.com>
2024-12-05 17:53:07 -0700

    Create NextImageWithConditionalSources.tsx

---
+++ source/utilities/next/NextImageWithConditionalSources.tsx
@@ -0,0 +1,37 @@
+import { getImageProps, ImageProps } from 'next/image';
+
+type NextImageWithConditionalSourcesProps = {
+    sources: {
+        path: string;
+        condition: string;
+    }[];
+} & Omit<ImageProps, 'src'>;
+const NextImageWithConditionalSources = ({ sources, ...props }: NextImageWithConditionalSourcesProps) => {
+    const baseProps = {
+        ...props,
+    };
+
+    if(sources.length <= 1) throw new Error('NextImageWithConditionalSources requires at least two sources.');
+
+    const { props: optimizedImgProps } = getImageProps({ ...baseProps, src: sources[0]!.path });
+
+    const optimizedSrcSets = sources.map((source) => {
+        const { props } = getImageProps({ ...baseProps, src: source.path });
+
+        console.log({ srcSet: props.srcSet, path: source.path });
+        return props;
+    });
+
+    return (
+        <picture>
+            {sources.map((source) => {
+                if(!source) return null;
+
+                return <source key={source.path} media={source.condition} srcSet={source.path} />;
+            })}
+            <img {...optimizedImgProps} />
+        </picture>
+    );
+};
+
+export default NextImageWithConditionalSources;
