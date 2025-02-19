6bc60cdc64192182a3daff1dd0f84aea20cf6099
Andrew Jacobs <38973832+magicjaqk@users.noreply.github.com>
2025-02-19 16:51:55 -0700

    Fixing useInView hook with drawer fixes in place.

--- source/utilities/react/useInView.ts
+++ source/utilities/react/useInView.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const useInView = (options: IntersectionObserverInit & { amount?: 'any' | 'all' } = {}) => {
+export const useInView = ({ root, ...options }: IntersectionObserverInit & { amount?: 'any' | 'all' } = {}) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any -- we don't know what the ref will be
     const ref = React.useRef<any>(null);
     const [inView, setInView] = React.useState<boolean | undefined>(undefined);
@@ -14,15 +14,23 @@ export const useInView = (options: IntersectionObserverInit & { amount?: 'any' |
             };
         }
 
+        let calculatedRoot = root;
+        if(!calculatedRoot) {
+            calculatedRoot = document.querySelector('main');
+        }
+
         // Otherwise, return a new IntersectionObserver
-        return new IntersectionObserver((entries) => {
-            const isIntersecting =
-                options.amount === 'all'
-                    ? entries.every((entry) => entry.isIntersecting)
-                    : entries.some((entry) => entry.isIntersecting);
-            setInView(isIntersecting);
-        }, options);
-    }, [options]);
+        return new IntersectionObserver(
+            (entries) => {
+                const isIntersecting =
+                    options.amount === 'all'
+                        ? entries.every((entry) => entry.isIntersecting)
+                        : entries.some((entry) => entry.isIntersecting);
+                setInView(isIntersecting);
+            },
+            { ...options, root: calculatedRoot },
+        );
+    }, [options, root]);
 
     React.useEffect(() => {
         if(ref.current) {
