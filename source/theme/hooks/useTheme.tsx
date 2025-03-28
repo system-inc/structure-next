bbda0d30ca0f7a7740e9b628fcd8c393cc002752
kirkouimet <kirk@kirkouimet.com>
2025-03-28 16:19:25 -0600

    Exposing ThemeClassName in useTheme hook

--- source/theme/hooks/useTheme.tsx
+++ source/theme/hooks/useTheme.tsx
@@ -1,14 +1,24 @@
 // Dependencies - Shared State
 import { useAtomValue } from 'jotai';
 import { readOnlyThemeAtom, readOnlyOperatingSystemThemeAtom } from '@structure/source/theme/ThemeProvider';
+import { Theme, OperatingSystemTheme, ThemeClassName } from '@structure/source/theme/ThemeTypes';
 
 // Hook - useTheme
 export function useTheme() {
     const theme = useAtomValue(readOnlyThemeAtom);
     const operatingSystemTheme = useAtomValue(readOnlyOperatingSystemThemeAtom);
+    const themeClassName =
+        theme === Theme.OperatingSystem
+            ? operatingSystemTheme === OperatingSystemTheme.Light
+                ? ThemeClassName.Light
+                : ThemeClassName.Dark
+            : theme === Theme.Light
+              ? ThemeClassName.Light
+              : ThemeClassName.Dark;
 
     return {
         theme: theme,
+        themeClassName: themeClassName,
         operatingSystemTheme: operatingSystemTheme,
     };
 }
