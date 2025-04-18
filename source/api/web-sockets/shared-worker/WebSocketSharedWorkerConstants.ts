bea403c62e829269f8ec8e6c4c6879ce82fd64ed
kirkouimet <kirk@kirkouimet.com>
2025-04-17 23:24:03 -0600

    Lots of improvements to web socket connections

--- source/api/web-sockets/shared-worker/WebSocketSharedWorkerConstants.ts
+++ source/api/web-sockets/shared-worker/WebSocketSharedWorkerConstants.ts
@@ -4,6 +4,8 @@
 export const SharedWorkerClientConnectionHeartbeatIntervalInMilliseconds = 3000; // How often to check if clients are still alive (3 seconds)
 export const SharedWorkerClientConnectionHeartbeatTimeoutInMilliseconds = 10000; // How long before considering a client disconnected (10 seconds)
 
-// WebSocket reconnection
+// WebSocket connection
+export const WebSocketConnectionTimeoutInMilliseconds = 15000; // Timeout for initial connection attempt (15 seconds)
 export const WebSocketReconnectDelayBaseInMilliseconds = 1000; // Base delay for WebSocket reconnection (1 second)
 export const WebSocketMaximumReconnectDelayInMilliseconds = 30000; // Maximum WebSocket reconnection delay (30 seconds)
+export const WebSocketPingIntervalInMilliseconds = 60000; // Send a ping every 60 seconds
