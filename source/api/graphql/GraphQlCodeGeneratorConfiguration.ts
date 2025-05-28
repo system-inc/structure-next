56751d10edf81d9172d07e9ba41f48eab90762cb
kirkouimet <kirk@kirkouimet.com>
2025-05-27 18:08:38 -0600

    Moving schema files into structure separate of project specific schema files

--- source/api/graphql/GraphQlCodeGeneratorConfiguration.ts
+++ source/api/graphql/GraphQlCodeGeneratorConfiguration.ts
@@ -4,16 +4,16 @@ import { graphql } from '../../../../../package.json';
 
 // Configuration
 export const graphQlCodeGeneratorConfiguration: GraphQlCodeGeneratorConfiguration = {
-    schema: graphql.projects.app.schema,
     generates: {
-        // Structure GraphQL files (using structure module documents)
+        // Structure GraphQL files (using structure schemas and documents)
         './libraries/structure/source/api/graphql/generated/': {
             preset: 'client',
-            documents: ['./libraries/structure/source/modules/**/api/**/*.graphql'],
+            schema: graphql.projects.structure.schema,
+            documents: graphql.projects.structure.documents,
             plugins: [
                 {
                     'typescript-operation-metadata': {
-                        metadata: ['./source/api/schemas/**/*.json'],
+                        metadata: ['./libraries/structure/source/api/graphql/schemas/**/*.json'],
                     },
                 },
             ],
@@ -21,10 +21,11 @@ export const graphQlCodeGeneratorConfiguration: GraphQlCodeGeneratorConfiguratio
                 useTypeImports: true,
             },
         },
-        // Project GraphQL files (using project module documents)
+        // Project GraphQL files (using project schemas and documents)
         './source/api/graphql/generated/': {
             preset: 'client',
-            documents: ['./source/modules/**/api/**/*.graphql'],
+            schema: graphql.projects.app.schema,
+            documents: graphql.projects.app.documents,
             plugins: [
                 {
                     'typescript-operation-metadata': {
