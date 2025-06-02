fb01cb74cb6bd939d3122f99aa1e23cbe23c4045
Andrew Jacobs <38973832+magicjaqk@users.noreply.github.com>
2025-06-02 10:14:11 -0600

    Updates to Account.ts and moving profile pages to their respective places in routing structure.

--- source/modules/account/Account.ts
+++ source/modules/account/Account.ts
@@ -8,12 +8,22 @@ import { AccountQuery } from '@project/source/api/GraphQlGeneratedCode';
 export const accountSignedInKey = ProjectSettings.identifier + 'AccountSignedIn';
 
 // Class - Account
+
 export class Account {
+    /** Account email address */
     emailAddress: AccountQuery['account']['emailAddress'] | null | undefined;
+    /** Account profile information */
     profile: AccountQuery['account']['profile'];
+    /** Access roles assigned to this account */
     accessRoles: AccountQuery['account']['accessRoles'];
+    /** Date when the account was created */
     createdAt: Date;
 
+    /**
+     * Creates a new Account instance
+     * @param accountCurrentQueryData Data from the account GraphQL query
+     * @throws Error if account data is invalid
+     */
     constructor(accountCurrentQueryData: AccountQuery['account']) {
         if(!accountCurrentQueryData) throw new Error('Invalid account data from GraphQL query.');
 
@@ -23,6 +33,37 @@ export class Account {
         this.accessRoles = accountCurrentQueryData.accessRoles;
     }
 
+    /**
+     * Gets the URL of the profile image
+     * @returns The URL of the profile image or undefined if not found
+     */
+    get profileImage() {
+        const image = this.profile?.images?.find((image) => image.variant === 'profile-image');
+
+        return image?.url;
+    }
+
+    /**
+     * Gets the display name for the profile
+     * @returns The profile display name or username prefixed with '@'
+     */
+    get profileDisplayName() {
+        return this.profile?.displayName || '@' + this.profile?.username;
+    }
+
+    /**
+     * Gets the date when the profile was created
+     * @returns Date object representing when the profile was created
+     */
+    get profileJoinedDate() {
+        return new Date(this.profile?.createdAt);
+    }
+
+    /**
+     * Gets the public display name for the account
+     * @deprecated Use the profileDisplayName getter instead
+     * @returns The public display name string
+     */
     getPublicDisplayName() {
         let publicDisplayName = '';
 
@@ -39,14 +80,28 @@ export class Account {
         return publicDisplayName;
     }
 
+    /**
+     * Checks if the account has a specific role
+     * @param role The role to check
+     * @returns True if the account has the specified role
+     */
     hasRole(role: string) {
         return this.accessRoles.includes(role);
     }
 
+    /**
+     * Checks if the account has any of the specified roles
+     * @param roles Array of roles to check against
+     * @returns True if the account has any of the specified roles
+     */
     hasAnyRole(roles: string[]) {
         return roles.some((role) => this.hasRole(role));
     }
 
+    /**
+     * Checks if the account has Administrator role
+     * @returns True if the account is an administrator
+     */
     isAdministator() {
         return this.hasRole('Administrator');
     }
