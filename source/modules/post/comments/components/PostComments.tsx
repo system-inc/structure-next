ef4e1eb2b30a48fd16ef82c7ecfa4f3765670b2d
kirkouimet <kirk@kirkouimet.com>
2025-11-29 07:34:27 -0700

    Clean up post comments until later

--- source/modules/post/comments/components/PostComments.tsx
+++ source/modules/post/comments/components/PostComments.tsx
@@ -4,13 +4,13 @@
 import React from 'react';
 
 // Dependencies - Main Components
-import { Button } from '@structure/source/components/buttons/Button';
+// import { Button } from '@structure/source/components/buttons/Button';
 // import { PostCommentControls } from '@structure/source/modules/post/comments/controls/PostCommentControls';
-import { TimeAgo } from '@structure/source/components/time/TimeAgo';
-import { ProfileLink } from '@structure/source/modules/post/components/ProfileLink';
+// import { TimeAgo } from '@structure/source/components/time/TimeAgo';
+// import { ProfileLink } from '@structure/source/modules/post/components/ProfileLink';
 
 // Dependencies - Assets
-import PlusIcon from '@structure/assets/icons/interface/PlusIcon.svg';
+// import PlusIcon from '@structure/assets/icons/interface/PlusIcon.svg';
 
 // Dependencies - Utilities
 import { mergeClassNames } from '@structure/source/utilities/style/ClassName';
@@ -23,26 +23,26 @@ export function PostComments(properties: PostCommentsProperties) {
     // Render the component
     return (
         <div id="comments" className={mergeClassNames('text-sm', properties.className)}>
-            <div>
-                <Button iconLeft={PlusIcon} className="pl-3">
-                    Add a Comment
+            {/* <div>
+                <Button variant="B" iconLeft={PlusIcon} className="pl-3">
+                    Add Comment
                 </Button>
-            </div>
-            <div className="mt-4">Sort and search comments</div>
+            </div> */}
+            {/* <div className="mt-4">Sort and search comments</div> */}
 
             <div className="mt-8">
                 {/* Comment Meta */}
-                <div className="flex items-center space-x-1.5">
+                {/* <div className="flex items-center space-x-1.5">
                     <ProfileLink username={'anonymous'} displayName={'Anonymous'} />
                     <div>&bull;</div>
                     <div className="content--2">
                         <TimeAgo startTimeInMilliseconds={new Date().getTime()} />
                     </div>
-                </div>
+                </div> */}
 
                 <div className="">
                     {/* Comment Content */}
-                    <div className="mt-3 ml-8">This is the first principle of our company.</div>
+                    {/* <div className="mt-3 ml-8">This is the first principle of our company.</div> */}
 
                     {/* Comment Controls */}
                     {/* <PostCommentControls
